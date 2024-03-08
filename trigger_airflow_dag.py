8c064ff02079567ba78277d17d3856ff409255d8
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-08 16:45:59 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -3,11 +3,11 @@ import os
 
 key_file = os.environ["airflow_instance_private_key"]
 #key_file = "/home/ubuntu/MLOps_Repo/backend/mlflow_as_a_service/test.pem"
-instance_ip = os.environ["airflow_instance_ip"]
+airflow_instance_ip = os.environ["airflow_instance_ip"]
 ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 key = paramiko.RSAKey.from_private_key_file(key_file)
-ssh_client.connect(hostname = instance_ip, username='ubuntu', pkey = key)
+ssh_client.connect(hostname = airflow_instance_ip, username='ubuntu', pkey = key)
 ftp_client=ssh_client.open_sftp()
 commands = ["docker exec df677905f4d2 airflow trigger_dag -c airflow-jenkins-dag refresh_dags"]
 
