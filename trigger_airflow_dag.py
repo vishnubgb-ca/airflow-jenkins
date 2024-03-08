9d372a4ecdbb194b1b1c1dab0e5b05d0ac30d1d9
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-08 15:27:34 +0530

    Create trigger_airflow_dag.py

---
+++ trigger_airflow_dag.py
@@ -0,0 +1,16 @@
+import paramiko
+import os
+
+key_file = os.environ["airflow_instance_private_key"]
+instance_ip = os.environ["airflow_instance_ip"]
+ssh_client=paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+key = paramiko.RSAKey.from_private_key_file(key_file)
+ssh_client.connect(hostname = instance_ip, username='ubuntu', pkey = key)
+ftp_client=ssh_client.open_sftp()
+commands = ["docker exec df677905f4d2 airflow trigger_dag -c airflow-jenkins-dag refresh_dags"]
+
+for cmd in commands:
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    print(stdout.read())
+    print(stderr.read())
