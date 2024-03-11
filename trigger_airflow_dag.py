ece0cca7f1e4e6d50c325ae655ee8fdab46d77a9
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-11 12:29:30 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -1,13 +1,13 @@
 import paramiko
 import os
 
-airflow_instance_private_key = os.environ["airflow_instance_private_key"]
-#key_file = "/home/ubuntu/MLOps_Repo/backend/mlflow_as_a_service/test.pem"
+#airflow_instance_private_key = os.environ["airflow_instance_private_key"]
+key_file = "/home/ubuntu/MLOps_Repo/backend/mlflow_as_a_service/test.pem"
 airflow_instance_ip = os.environ["airflow_instance_ip"]
 print(airflow_instance_ip)
 ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-key = paramiko.RSAKey.from_private_key_file('test.pem')
+key = paramiko.RSAKey.from_private_key_file(key_file)
 print("connecting to instance........")
 ssh_client.connect(hostname = '172.31.61.199', username='ubuntu', pkey = key)
 print("connected")
