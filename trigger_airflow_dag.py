31fb6bb3adab22b5cea9c1cb221dd95360f541bb
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-11 11:44:54 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -9,7 +9,7 @@ ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 key = paramiko.RSAKey.from_private_key_file(airflow_instance_private_key)
 print("connecting to instance........")
-ssh_client.connect(hostname = airflow_instance_ip, username='ubuntu', pkey = key)
+ssh_client.connect(hostname = '172.31.61.199', username='ubuntu', pkey = key)
 print("connected")
 ftp_client=ssh_client.open_sftp()
 commands = ["docker exec df677905f4d2 airflow trigger_dag -c airflow-jenkins-dag refresh_dags"]
