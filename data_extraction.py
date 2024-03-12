cb473462a261f737ff6eeffbf4b88ea0aeb2cb40
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-12 15:07:27 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import os
 
-def load_data():
-    data = pd.read_csv('data.csv')
-    print(data.head())
-    return data
 
-load_data()
+data = pd.read_csv('data.csv')
+print(data.head())
+    
