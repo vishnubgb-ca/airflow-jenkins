a930219f417c6d52ab12fc060393475adac864f6
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-14 16:40:17 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -11,7 +11,7 @@ print("connecting to instance........")
 ssh_client.connect(hostname = airflow_instance_ip, username='ubuntu', pkey = key)
 print("connected")
 ftp_client=ssh_client.open_sftp()
-commands = ["sudo docker exec 4fbb5ce5ad8d airflow dags trigger airflowJenkinsDag"]
+commands = ["sudo docker exec 4fbb5ce5ad8d airflow dags trigger airflow-jenkins"]
 
 for cmd in commands:
     stdin, stdout, stderr = ssh_client.exec_command(cmd)
