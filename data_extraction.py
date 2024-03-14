127ecc1a473391cfe04c7a6a4ae4f3aa87429210
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-14 12:38:48 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import os
+import boto3
 
-
-data = pd.read_csv('data.csv')
-print(data.head())
+#data = pd.read_csv('data.csv')
+#print(data.head())
     
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='us-east-1',
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key
+)
