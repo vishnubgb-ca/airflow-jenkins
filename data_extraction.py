2d07eaeeb6a6e00127628e34d939485e805043a8
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-14 13:15:17 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -11,10 +11,21 @@ def read_csv_from_s3():
     s3 = boto3.client('s3', aws_access_key_id=access_key,
                       aws_secret_access_key=secret_key,
                       region_name='us-east-1')
- 
     obj = s3.get_object(Bucket='mlanglesdev', Key='Student_Performance_Classifier/data.csv')
     data = pd.read_csv(obj['Body'])
     print(data.head())
     return data
 
-read_csv_from_s3()
+def upload_csv_to_s3(data, object_key):
+    #data = pd.read_csv(csv_file_path)
+    s3 = boto3.client('s3', aws_access_key_id=access_key,
+                      aws_secret_access_key=secret_key,
+                      region_name='us-east-1')
+    
+    csv_buffer = data.to_csv(index=False)
+    s3.put_object(Bucket='mlanglesdev', Key=object_key, Body=csv_buffer)
+    print(f"CSV file uploaded to S3://{bucket_name}/{object_key}")
+
+data = read_csv_from_s3()
+object_key = 'Student_Performance_Classifier/rawdata.csv'
+upload_csv_to_s3(data, object_key)
