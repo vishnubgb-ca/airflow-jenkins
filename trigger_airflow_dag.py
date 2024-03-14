68f5cfec9e3e1cb74668d5798fefdd15c1a64ee9
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-03-14 16:39:59 +0530

    Update trigger_airflow_dag.py

--- trigger_airflow_dag.py
+++ trigger_airflow_dag.py
@@ -1,12 +1,12 @@
 import paramiko
 import os
 
-#airflow_instance_private_key = os.environ["airflow_instance_private_key"]
+airflow_instance_private_key = os.environ["airflow_instance_private_key"]
 airflow_instance_ip = os.environ["airflow_instance_ip"]
 print(airflow_instance_ip)
 ssh_client=paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-key = paramiko.RSAKey.from_private_key_file('test.pem')
+key = paramiko.RSAKey.from_private_key_file(airflow_instance_private_key)
 print("connecting to instance........")
 ssh_client.connect(hostname = airflow_instance_ip, username='ubuntu', pkey = key)
 print("connected")
